3a3dbb02c5185ad0019f229cb86c297501849088
gcb62 <46456419+gcb62@users.noreply.github.com>
2019-02-24 11:52:25 -0500

    Parsing responses

--- robo_advisory.py
+++ robo_advisory.py
@@ -1,19 +1,35 @@
 # Robo_Advisor.py
 
-# app/robo_advisor.py
-
 # TODO: import some modules and/or packages here
-
 # TODO: write some Python code here to produce the desired functionality...
 
-print("-----------------------)
+#formulate inputs
+
+import requests
+import json
+
+# using JSON not CSV
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+response = requests.get(request_url)
+# print(type(response))
+# print(response.status_code)
+# print(response.text)
+
+parsed_response = json.loads(response.text)
+
+
+breakpoint()
+
+
+#outputs below
+
+print("-----------------------")
 print("STOCK SYMBOL: AMZN")
 
-print("-----------------------)
+print("-----------------------")
 print("CRUNCHING THE DATA...")
 
-print("-----------------------)
+print("-----------------------")
 print("LATEST CLOSING PRICE: $1,259.19")
 
 # ... etc.
-
